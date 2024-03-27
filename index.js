35ae2204e0faec238fea7f905777ab6ad0338def
Нафтолин Владислав Феликсович <v.f.naftolin@cardio.by>
2024-03-27 13:54:28 +0300

    implementation of mongo into back

--- index.js
+++ index.js
@@ -1,36 +1,20 @@
-const { request, response } = require("express");
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const Note = require(`./models/note`);
+
 
 app.use(cors());
 app.use(express.json());
 app.use((express.static(`dist`)));
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
-  },
-];
-
-const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
+// const generateId = () => {
+//   const maxId = notes.length > 0
+//     ? Math.max(...notes.map(n => n.id))
+//     : 0
+//   return maxId + 1
+// }
 
 app.post("/api/notes", (request, response) => {
   const body = request.body;
@@ -41,25 +25,19 @@ app.post("/api/notes", (request, response) => {
     })
   }
 
-  const note = {
-    content: body.content,
-    important: Boolean(body.important) || false,
-    id: generateId(),
-  }
+  const note = new Note({ content: body.content,
+    important: body.important || false,
+  });
 
-  notes = notes.concat(note);
-
-  response.json(note);
+  note.save().then(savedNote => {
+    response.json(savedNote);
+  });
 })
 
 app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => note.id === id);
-  if (note) {
-    response.json(note);
-  } else {
-    response.status(404).end();
-  }
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
 });
 
 app.get("/", (request, response) => {
@@ -67,7 +45,10 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/notes", (request, response) => {
-  response.json(notes);
+  Note.find({}).then(notes => {
+    console.log(notes);
+    response.json(notes);
+  })
 });
 
 app.delete("api/notes/:id", (request,response) => {
@@ -77,7 +58,7 @@ app.delete("api/notes/:id", (request,response) => {
     response.status(204).end();
 })
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
